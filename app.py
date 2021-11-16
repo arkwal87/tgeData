65cbb5d4ee8b8e15be96326fdf4a057ef0fbe408
arkwal87 <66551473+arkwal87@users.noreply.github.com>
2021-11-16 15:03:05 +0100

    dodane kontrkaty gazowe - termin

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from main import tgePrice
-from term import holidays, termPrice
+from term import holidays, termPrice, termGasPrice
 from tgeBASE import tgeBasePrice
 from gas import tgeGasPrice
 
@@ -10,5 +10,6 @@ tradeYear = 2021
 
 tgePrice(startDay, endDay, tradeMonth, tradeYear)
 termPrice(startDay, endDay, tradeMonth, tradeYear)
+termGasPrice(startDay, endDay, tradeMonth, tradeYear)
 tgeBasePrice(startDay, endDay, tradeMonth, tradeYear)
 tgeGasPrice(startDay, endDay, tradeMonth, tradeYear)
--- term.py
+++ term.py
@@ -23,9 +23,9 @@ holidays = [
 
 holidays = list(map(lambda singleDate: datetime.strptime(singleDate, '%d-%m-%Y').date().strftime('%Y-%m-%d'), holidays))
 
-startDay = 17
-endDay = 20
-tradeMonth = 3
+startDay = 15
+endDay = 16
+tradeMonth = 11
 tradeYear = 2021
 
 
@@ -72,3 +72,51 @@ def termPrice(startDay, endDay, tradeMonth, tradeYear):
             df.index.name = tradeDate
             print(conv_date)
             df.to_excel(f"C:/Users/{os.environ['USERNAME']}/Documents/pyData/FW/{conv_date}_FW.xlsx")
+
+
+def termGasPrice(startDay, endDay, tradeMonth, tradeYear):
+    for tradeDay in range(startDay, endDay + 1):
+        if int(tradeDay) < 10:
+            tradeDay = str("0" + str(int(tradeDay)))
+        if int(tradeMonth) < 10:
+            tradeMonth = str("0" + str(int(tradeMonth)))
+        tradeDate = f'{tradeDay}-{tradeMonth}-{tradeYear}'
+        conv_date = datetime.strptime(tradeDate, '%d-%m-%Y').date()
+        if conv_date.isoweekday() in range(1, 6) and str(conv_date) not in holidays:
+            my_page = requests.get(f'https://tge.pl/gaz-otf?dateShow={tradeDate}&dateAction=prev')
+            # print(f'https://tge.pl/energia-elektryczna-otf?dateShow={tradeDate}&dateAction=prev')
+            soup = BeautifulSoup(my_page.content, 'html.parser')
+            new = soup.find_all("tr", class_='period-')
+            data_set = []
+            keys = [
+                "Instrument"
+                "First trade",
+                "Settlement price",
+                "Min Price",
+                "Max Price",
+                "Total Volume",
+                "Lots",
+                "Total Value",
+                "Number of transactions",
+                "Total number of open interests"
+            ]
+            for element in new:
+                new_array = [
+                    element.select("td", class_='footable-visible footable-first-column col-instrument_name')[
+                        0].get_text()]
+                # print(new_array)
+                for key in keys:
+                    textVal = element.select(f"td:nth-child({keys.index(key) + 3})")[0].get_text().replace(",", ".")
+                    if textVal != "-":
+                        textVal = float(textVal)
+                    new_array.append(textVal)
+                data_set.append(new_array)
+            df = pd.DataFrame(data=data_set)
+            df.set_index(df.columns[0], inplace=True)
+            df.columns = [key for key in keys]
+            df.index.name = tradeDate
+            print(conv_date)
+            df.to_excel(f"C:/Users/{os.environ['USERNAME']}/Documents/pyData/gasFW/gas_{conv_date}_FW.xlsx")
+
+
+termGasPrice(startDay, endDay, tradeMonth, tradeYear)
